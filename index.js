d81ee13c8b67ac8e2acbf895225067d4e64f8543
Mohit Choyal <72176576+mohitchoyal2002@users.noreply.github.com>
2021-12-11 15:58:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// var name = prompt("What is your name: ");
+// alert(name);
+var randomNumber = Math.floor(Math.random()*6)+1;
+// console.log(randomNumber);
+
+document.querySelector(".img1").setAttribute("src","images/dice3.png");
